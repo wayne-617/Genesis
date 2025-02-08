4f2fcec9b7c0d6833ac227209cfc889139a8e089
RTN4596 <rtn05056@uga.edu>
2025-02-08 13:35:36 -0500

    Fixed text input color of signup

--- ugahacks-app/app/pages/signup/page.tsx
+++ ugahacks-app/app/pages/signup/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const SignupPage: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white text-white-800 rounded-lg shadow-lg p-8 w-96">
+      <div className="bg-white text-gray-800 rounded-lg shadow-lg p-8 w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">Sign Up</h2>
         <label className="input input-bordered flex items-center gap-2 mb-4">
           <svg
@@ -18,7 +18,7 @@ const SignupPage: React.FC = () => {
             <path
               d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type="text" className="grow p-2" placeholder="Email" />
+          <input type="text" className="grow p-2 text-gray-400" placeholder="Email" />
         </label>
         <label className="input input-bordered flex items-center gap-2 mb-4">
           <svg
@@ -29,7 +29,7 @@ const SignupPage: React.FC = () => {
             <path
               d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
-          <input type="text" className="grow p-2" placeholder="Username" />
+          <input type="text" className="grow p-2 text-gray-400" placeholder="Username" />
         </label>
         <label className="input input-bordered flex items-center gap-2 mb-4">
           <svg
@@ -42,7 +42,7 @@ const SignupPage: React.FC = () => {
               d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
               clipRule="evenodd" />
           </svg>
-          <input type="password" className="grow p-2" placeholder="Password" />
+          <input type="password" className="grow p-2 text-gray-400" placeholder="Password" />
         </label>
         <button className="btn btn-primary w-full">Sign Up</button>
       </div>
