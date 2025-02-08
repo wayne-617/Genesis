f10503c1e229bec146f5bef8e350401e6193641b
DuckDonni <61603826+DuckDonni@users.noreply.github.com>
2025-02-08 12:50:40 -0500

    developed base pages

---
+++ ugahacks-app/app/pages/home/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Home</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
---
+++ ugahacks-app/app/pages/profile/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">Profile</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;
