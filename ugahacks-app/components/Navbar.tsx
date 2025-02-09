08acf6f7048ae08f3b02fddbce57b8eac0d3e20a
RTN4596 <rtn05056@uga.edu>
2025-02-09 07:43:37 -0500

    Added styling and format to home page
    Made home page and filled in the about section.

--- ugahacks-app/components/Navbar.tsx
+++ ugahacks-app/components/Navbar.tsx
@@ -36,7 +36,7 @@ const Navbar: React.FC = () => {
         </div>
       </div>
       <div className="navbar-center">
-        <h1><a className="btn btn-ghost text-xl text-white" onClick={() => router.push('/pages/home')}>Genesis</a></h1>
+        <h1><a className="btn btn-ghost text-5xl text-white" onClick={() => router.push('/pages/home')}>Genesis</a></h1>
       </div>
       <div className="navbar-end space-x-4">
         <button className='btn btn-outline btn-secondary' onClick={() => router.push('/pages/signup')}>Signup</button>
